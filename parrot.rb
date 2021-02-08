25941c58dee8d90698c880562ae214160a09c59e
Bianca <biancancanal@gmail.com>
2021-02-08 13:10:00 -0600

    commit tree 1c8fbeefc746471e76b52e96cf7b2adfd3dfc0b8

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(word="Squawk!") 
+    phrase = "#{word}"
+    puts phrase
+    return phrase
+end
+
+parrot("Hello")
+parrot
